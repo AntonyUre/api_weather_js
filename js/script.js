b0f004c912a800bfbee40af22df0566bd034cddd
AntonyUre <ureta.art@hotmail.com>
2022-07-21 16:48:42 -0500

    metodo ben correcto

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 const MAPS_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
 // Contenedores
-const temp = document.querySelector(".container__cards");
+const temp = document.querySelector(".card__date");
 const form = document.getElementById("search-form");
 const searchBox = document.getElementById("searchbox");
 const boxMap = document.querySelector(".right");
@@ -37,39 +37,39 @@ function onSubmit(event) {
 
 form.addEventListener("submit", onSubmit, true);
 
-function printMap(r) {
+function printMap(res) {
   console.log("printMap");
-  let lat = r.data.coord.lat;
+  let lat = res.data.coord.lat;
   console.log(lat);
-  let lon = r.data.coord.lon;
+  let lon = res.data.coord.lon;
   console.log(lon);
   let ifrm = document.createElement("iframe");
   ifrm.setAttribute(
     "src",
-    `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3172.1368882059437!2d${lon}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2spe!4v1658421521167!5m2!1ses!2spe`
+    `https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d62414.9511018945!2d${lon}!3d${lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2spe!4v1658433513389!5m2!1ses-419!2spe`
   );
   boxMap.innerHTML = "";
   boxMap.appendChild(ifrm);
 }
 
 // Imprimir la informacion del clima
-function printCard(r) {
+function printCard(res) {
   temp.innerHTML = `
   <div class="card__item">
     <div class="card__img-container">
-    <div><h2>${r.data.main.temp}° C</h2></div>
-      <img class="card__logo" src="http://openweathermap.org/img/wn/${r.data.weather[0].icon}@2x.png" alt="Logo LarnU" />
+    <div><h2>${res.data.main.temp}° C</h2></div>
+      <img class="card__logo" src="http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png" alt="Logo LarnU" />
     </div>
     <div class="card__text-container">
-      <div><h2>${r.data.name} - ${r.data.sys.country}</h2></div>
-      <div><h2>H ${r.data.main.humidity}</h2></div>
-      <div><h2>Temperatura${r.data.weather[0].description}</h2></div>
+      <div><h2>${res.data.name} - ${res.data.sys.country}</h2></div>
+      <div><h2>H ${res.data.main.humidity}</h2></div>
+      <div><h2>Temperatura${res.data.weather[0].description}</h2></div>
     </div>
   </div>`;
-  printMap(r);
+  printMap(res);
 }
 
-let time = async () => {
-  let r = await getWeather();
-  printCard(r);
+const time = async () => {
+  const res = await getWeather();
+  printCard(res);
 };
